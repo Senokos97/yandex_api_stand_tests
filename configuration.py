dfb03d645213e45105798133fb7ab2158071dc3b
Senokos <viki.tori78@gmail.com>
2023-11-16 13:57:04 +0300

    first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://f8a43a29-5aa9-4cff-9de3-9efabdf6472c.serverhub.praktikum-services.ru/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
